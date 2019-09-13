d82b3252cdca779456b5c3aed403407d3dbba521
Alexandre Chanson <a.chanson.tours@gmail.com>
2019-09-13 16:34:07 +0200

    added some TODO off for the weekend

--- src/main/java/com/olap3/cubeexplorer/optimize/BudgetManager.java
+++ src/main/java/com/olap3/cubeexplorer/optimize/BudgetManager.java
@@ -2,8 +2,8 @@ package com.olap3.cubeexplorer.optimize;
 
 import com.olap3.cubeexplorer.info.InfoCollector;
 
-import java.util.List;
-import java.util.Set;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public interface BudgetManager {
     /**
@@ -13,4 +13,25 @@ public interface BudgetManager {
      * @return
      */
     public Set<InfoCollector> findPossibleSet(List<InfoCollector> candidates, int timeBudget);
+
+
+
+    // TODO
+    /**
+     * Given an existing solution and a search space of info collectors find all equivalent solution by swapping 'identical'
+     * items in the solution.
+     * 'identical' is defined using time estimation and the metric, two ic are considered equal if they have equal time and metric value
+     * @param solution
+     * @param metric
+     * @return
+     */
+    static Set<Set<InfoCollector>> expandToEqSolutions(List<InfoCollector> serchSpace, Set<InfoCollector> solution, AprioriMetric metric){
+
+        serchSpace.sort(Comparator.comparingDouble(metric::rate));
+        Map<Double, List<InfoCollector>> groups = serchSpace.stream().collect(Collectors.groupingBy(metric::rate));
+
+        //TODO look for swapping candidates within groups ...
+
+        return null;
+    }
 }
--- src/main/java/com/olap3/cubeexplorer/optimize/TemplateGenerator.java
+++ src/main/java/com/olap3/cubeexplorer/optimize/TemplateGenerator.java
@@ -1,4 +1,5 @@
 package com.olap3.cubeexplorer.optimize;
 
 public class TemplateGenerator implements PlanGenerator {
+    //TODO See this with panos
 }
