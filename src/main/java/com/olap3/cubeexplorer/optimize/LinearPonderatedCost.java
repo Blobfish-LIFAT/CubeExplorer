f75be38b58d4fd6dbd9cdf00e60ee4a3ff5439cd
Alexandre Chanson <a.chanson.tours@gmail.com>
2019-09-17 14:17:44 +0200

    added basic cost model for adjusting estimates

---
+++ src/main/java/com/olap3/cubeexplorer/optimize/LinearPonderatedCost.java
@@ -0,0 +1,28 @@
+package com.olap3.cubeexplorer.optimize;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LinearPonderatedCost implements CostModel{
+    List<Double> factors;
+
+    public LinearPonderatedCost() {
+        factors = new ArrayList<>();
+    }
+
+    @Override
+    public void updateCost(TimeableOp operation) {
+        double factor = ((double)operation.aposterioriTime())/operation.aprioriTime();
+        factors.add(factor);
+    }
+
+    @Override
+    public int correctedEstimate(TimeableOp operation) {
+        if (factors.size() == 0)
+            return operation.aprioriTime();
+        else {
+            double factor = factors.stream().mapToDouble(Double::doubleValue).sum()/factors.size();
+            return (int) Math.round(factor * operation.aprioriTime());
+        }
+    }
+}
